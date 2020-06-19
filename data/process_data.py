b92f7db63944d9f58d08fa2c1588b6ae22ee5cf5
Liliya Jeromin <zl108108@gmail.com>
2020-06-19 19:07:41 +0200

    all done but cannot check the .py files

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,67 @@
 import sys
-
+import pandas as pd
+import sqlite3
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """Load dataframe from filepaths
+    INPUT
+    messages_filepath - str, link to file
+    categories_filepath - str, link to file
+    OUTPUT
+    df - pandas DataFrame
+    """
+    messages_filepath = 'disaster_messages.csv'
+    categories_filepath = 'disaster_categories.csv'
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    '''
+    Clean the data
+    
+    INPUT - a data frame with merged messages and categories
+    
+    OUTPUT
+    df that has following charachteristics:
+    1. The column categories is splited into 36 different categories
+    2. The columns are renamed acordingly
+    3. Values for each column are int without strings
+    4. Has no column 'categories'
+    5. Has id, messages, original, genre and 36 columns with feautures
+    6. Has no duplicates
+    
+    '''
+    
+    categories = df['categories'].str.split(';', expand=True)
+    row = categories.iloc[0]
+    category_colnames = row.str.split('-').apply(lambda x: x[0])
+    categories.columns = category_colnames
+    
+    for column in categories:
+    # set each value to be the last character of the string
+        categories[column] = categories[column].str.split('-').apply(lambda x: x[1])
+    
+    #convert column from string to numeric
+    categories = categories.astype(int) 
+    
+    df = df.drop(columns='categories')
+    df = pd.concat([df, categories], axis=1)
+    df = df.drop_duplicates()
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    '''
+    Saves df into database
+    
+    '''
+    database_filename = 'sqlite:///DisasterResponse.db'
+    engine = create_engine(database_filename)
+    df.to_sql('df', engine, index=False)
 
 
 def main():
